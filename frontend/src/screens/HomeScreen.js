4aa77b6cf8b35dcb51c12103f2e114bdacdd4c6a
Catevika <dominique.bello@outlook.fr>
2020-10-02 16:01:36 +0200

    Add HomeScreen and Product components

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+import products from '../products';
+
+const HomeScreen = () => {
+	return (
+		<>
+			<Row>
+				{products.map((product) => {
+					return (
+						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+							<Product product={product} />
+						</Col>
+					);
+				})}
+			</Row>
+		</>
+	);
+};
+
+export default HomeScreen;

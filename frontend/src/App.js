e2469f7f01fa7868356e66711c88c793c47ce423
Catevika <dominique.bello@outlook.fr>
2020-10-05 14:22:04 +0200

    Add ProductScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 			<main className='py-3'>
 				<Container>
 					<Route path='/' component={HomeScreen} exact />
-					<Route path='/:id' component={ProductScreen} />
+					<Route path='/product/:id' component={ProductScreen} />
 				</Container>
 			</main>
 			<Footer />

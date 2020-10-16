2279906ca16beae88188e68295f021ff0fbfe662
Catevika <dominique.bello@outlook.fr>
2020-10-16 17:59:22 +0200

    Update username when logged in and add logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import {
 	USER_LOGIN_REQUEST,
 	USER_LOGIN_SUCCESS,
-	USER_LOGIN_FAIL
+	USER_LOGIN_FAIL,
+	USER_LOGOUT
 } from '../constants/userConstants';
 
 export const login = (email, password) => async (dispatch) => {
@@ -38,4 +39,9 @@ export const login = (email, password) => async (dispatch) => {
 					: error.message
 		});
 	}
+};
+
+export const logout = () => (dispatch) => {
+	localStorage.removeItem('userInfo');
+	dispatch({ type: USER_LOGOUT });
 };

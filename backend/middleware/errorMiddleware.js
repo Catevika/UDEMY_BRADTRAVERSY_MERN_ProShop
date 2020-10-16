e02b8c3ac9a7010007814c33abd9c0a8b49fa9e3
Catevika <dominique.bello@outlook.fr>
2020-10-16 15:08:55 +0200

    Add user registration and password encryption

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,5 +1,5 @@
-const notFound = (err, req, res, next) => {
-	const error = new Error(`Not found - ${req.originalUrl}`);
+const notFound = (req, res, next) => {
+	const error = new Error(`Not Found - ${req.originalUrl}`);
 	res.status(404);
 	next(error);
 };

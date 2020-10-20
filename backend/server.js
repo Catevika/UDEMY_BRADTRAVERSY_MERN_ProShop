dd3d51be01048e11acef51e62a34abfb479f7c84
Catevika <dominique.bello@outlook.fr>
2020-10-20 12:18:05 +0200

    Add Order controller and routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import { config } from 'dotenv';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);

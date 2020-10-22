9a19a06977da32f9fcf0a282d34ac6784f2ff33f
Catevika <dominique.bello@outlook.fr>
2020-10-22 18:14:22 +0200

    Add Order list to Profile

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -15,6 +15,7 @@ import {
 import {
 	orderCreateReducer,
 	orderDetailsReducer,
+	orderListMyReducer,
 	orderPayReducer
 } from './reducers/orderReducers';
 
@@ -28,7 +29,8 @@ const reducer = combineReducers({
 	userUpdateProfile: userUpdateProfileReducer,
 	orderCreate: orderCreateReducer,
 	orderDetails: orderDetailsReducer,
-	orderPay: orderPayReducer
+	orderPay: orderPayReducer,
+	orderListMy: orderListMyReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')

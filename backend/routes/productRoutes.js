d1d267ffe28ef095ca80027e804a93813a5a7706
Catevika <dominique.bello@outlook.fr>
2020-10-27 18:56:18 +0100

    Add Admin Delete Product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,17 @@
 import express from 'express';
+
 import {
 	getProducts,
-	getProductById
+	getProductById,
+	deleteProduct
 } from '../controllers/productController.js';
+
+import { admin, protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').get(getProducts);
 
-router.route('/:id').get(getProductById);
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;

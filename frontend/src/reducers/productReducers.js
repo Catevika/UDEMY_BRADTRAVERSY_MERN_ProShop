f2827ae7fb2dcacba831aae5a3302d39693fec68
Catevika <dominique.bello@outlook.fr>
2020-10-29 11:42:05 +0100

    Add Admin Update Product

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -5,13 +5,18 @@ import {
 	PRODUCT_DETAILS_REQUEST,
 	PRODUCT_DETAILS_SUCCESS,
 	PRODUCT_DETAILS_FAIL,
+	PRODUCT_DETAILS_RESET,
 	PRODUCT_DELETE_REQUEST,
 	PRODUCT_DELETE_SUCCESS,
 	PRODUCT_DELETE_FAIL,
 	PRODUCT_CREATE_REQUEST,
 	PRODUCT_CREATE_SUCCESS,
 	PRODUCT_CREATE_FAIL,
-	PRODUCT_CREATE_RESET
+	PRODUCT_CREATE_RESET,
+	PRODUCT_UPDATE_REQUEST,
+	PRODUCT_UPDATE_SUCCESS,
+	PRODUCT_UPDATE_FAIL,
+	PRODUCT_UPDATE_RESET
 } from '../constants/productConstants';
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -38,6 +43,8 @@ export const productDetailsReducer = (
 			return { loading: false, product: action.payload };
 		case PRODUCT_DETAILS_FAIL:
 			return { loading: false, error: action.payload };
+		case PRODUCT_DETAILS_RESET:
+			return { product: { reviews: [] } };
 		default:
 			return state;
 	}
@@ -66,6 +73,21 @@ export const productCreateReducer = (state = {}, action) => {
 			return { loading: false, error: action.payload };
 		case PRODUCT_CREATE_RESET:
 			return {};
+		default:
+			return state;
+	}
+};
+
+export const productUpdateReducer = (state = { product: {} }, action) => {
+	switch (action.type) {
+		case PRODUCT_UPDATE_REQUEST:
+			return { loading: true };
+		case PRODUCT_UPDATE_SUCCESS:
+			return { loading: false, success: true, product: action.payload };
+		case PRODUCT_UPDATE_FAIL:
+			return { loading: false, error: action.payload };
+		case PRODUCT_UPDATE_RESET:
+			return { product: {} };
 		default:
 			return state;
 	}
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -5,7 +5,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer';
-import { listProductsDetails } from '../actions/productActions';
+import { listProductsDetails, updateProduct } from '../actions/productActions';
+import {
+	PRODUCT_UPDATE_RESET,
+	PRODUCT_DETAILS_RESET
+} from '../constants/productConstants';
 
 const ProductEditScreen = ({ match, history }) => {
 	const productId = match.params.id;
@@ -22,23 +26,47 @@ const ProductEditScreen = ({ match, history }) => {
 	const productDetails = useSelector((state) => state.productDetails);
 	const { loading, error, product } = productDetails;
 
+	const productUpdate = useSelector((state) => state.productUpdate);
+	const {
+		loading: loadingUpdate,
+		error: errorUpdate,
+		success: successUpdate
+	} = productUpdate;
+
 	useEffect(() => {
-		if (!product.name || product._id !== productId) {
-			dispatch(listProductsDetails(productId));
+		if (successUpdate) {
+			dispatch({ type: PRODUCT_UPDATE_RESET });
+			dispatch({ type: PRODUCT_DETAILS_RESET });
+			history.push('/admin/productlist');
 		} else {
-			setName(product.name);
-			setPrice(product.price);
-			setImage(product.image);
-			setBrand(product.brand);
-			setCategory(product.category);
-			setCountInStock(product.countInStock);
-			setDescription(product.description);
+			if (!product.name || product._id !== productId) {
+				dispatch(listProductsDetails(productId));
+			} else {
+				setName(product.name);
+				setPrice(product.price);
+				setImage(product.image);
+				setBrand(product.brand);
+				setCategory(product.category);
+				setCountInStock(product.countInStock);
+				setDescription(product.description);
+			}
 		}
-	}, [history, dispatch, product, productId]);
+	}, [history, dispatch, product, productId, successUpdate]);
 
 	const submitHandler = (e) => {
 		e.preventDefault();
-		// TODO Update product
+		dispatch(
+			updateProduct({
+				_id: productId,
+				name,
+				price,
+				image,
+				brand,
+				category,
+				countInStock,
+				description
+			})
+		);
 	};
 
 	return (
@@ -48,6 +76,8 @@ const ProductEditScreen = ({ match, history }) => {
 			</Link>
 			<FormContainer>
 				<h1>Edit Product</h1>
+				{loadingUpdate && <Loader />}
+				{errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
 				{loading ? (
 					<Loader />
 				) : error ? (

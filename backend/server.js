4539e08e9868986fd54329c49fd05430696f39f7
Catevika <dominique.bello@outlook.fr>
2020-10-29 15:12:02 +0100

    Add Image Upload Config & Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
+import path from 'path';
 import express from 'express';
 import { config } from 'dotenv';
 import colors from 'colors';
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-import connectDB from './config/db.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 config();
 
@@ -19,9 +22,14 @@ app.get('/', (req, res) => {
 	res.send('API is running...');
 });
 
+const __dirname = path.resolve();
+
+app.use('/upload', express.static(path.join(__dirname, '/uploads')));
+
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.use(notFound);
 app.use(errorHandler);

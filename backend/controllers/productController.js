bdbeb77084613ca0068a7efebfca1a3528893559
Catevika <dominique.bello@outlook.fr>
2020-10-30 17:54:40 +0100

    Add Product Search

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,7 +7,15 @@ import asyncHandler from 'express-async-handler';
  * * @access  Public
  **/
 const getProducts = asyncHandler(async (req, res) => {
-	const products = await Product.find({});
+	const keyword = req.query.keyword
+		? {
+				name: {
+					$regex: req.query.keyword,
+					$options: 'i'
+				}
+		  }
+		: {};
+	const products = await Product.find({ ...keyword });
 	res.json(products);
 });
 

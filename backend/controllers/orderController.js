902a245a0b569159e93ae0dbc25147bb023b50b0
Catevika <dominique.bello@outlook.fr>
2020-10-30 14:36:52 +0100

    Add Mark as delivered button and functionalities

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -82,6 +82,26 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 });
 
 /**
+ * * @desc    Update order to delivered
+ * * @route   GET '/api/orders/:id/deliver'
+ * * @access  Private/admin
+ **/
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+	const order = await Order.findById(req.params.id);
+
+	if (order) {
+		order.isDelivered = true;
+		order.deliveredAt = Date.now();
+
+		const updatedOrder = await order.save();
+		res.json(updatedOrder);
+	} else {
+		res.status(404);
+		throw new Error('Order not found');
+	}
+});
+
+/**
  * * @desc    Get logged in user orders
  * * @route   GET '/api/orders/myorders'
  * * @access  Private
@@ -105,6 +125,7 @@ export {
 	addOrderItems,
 	getOrderById,
 	updateOrderToPaid,
+	updateOrderToDelivered,
 	getMyOrders,
 	getOrders
 };

610797cedb6197d837876110a566d88c9d3e2878
Catevika <dominique.bello@outlook.fr>
2020-10-31 16:04:03 +0100

    Add setup for production

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 import path from 'path';
 import express from 'express';
-import { config } from 'dotenv';
+import dotenv from 'dotenv';
 import colors from 'colors';
 import morgan from 'morgan';
-import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 
-config();
+dotenv.config();
 
 connectDB();
 
@@ -23,14 +23,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('API is running...');
-});
-
-const __dirname = path.resolve();
-
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -40,6 +32,21 @@ app.get('/api/config/paypal', (req, res) =>
 	res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running....');
+	});
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
@@ -48,6 +55,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.bold
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
 	)
 );
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -179,8 +179,12 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
 			payload: data
 		});
 
+		dispatch({
+			type: USER_LOGIN_SUCCESS,
+			payload: data
+		});
+
 		localStorage.setItem('userInfo', JSON.stringify(data));
-		// TODO CHANGE userInfo in the header on profile update
 	} catch (error) {
 		dispatch({
 			type: USER_UPDATE_PROFILE_FAIL,
